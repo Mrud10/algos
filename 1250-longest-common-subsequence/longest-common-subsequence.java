22163fce9a703dc2d035285bd7ed8b98b04210e7
Mrudul Sakharkar <mrudul.sakharkar23@vit.edu>
2026-07-31 13:13:08 +0530

    Time: 24 ms (34.23%) | Memory: 54.3 MB (6.63%) - LeetSync

---
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int [][] dp = new int[text1.length()][text2.length()];
+        for (int[] row : dp) {
+        Arrays.fill(row, -1);
+        }
+        return maxLength(text1,text2,text1.length()-1,text2.length()-1,dp);
+    }
+    public int maxLength(String s1 , String s2,int i, int j,int[][]dp){
+        if(i<0 || j<0) return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+        if(s1.charAt(i)==s2.charAt(j)){
+            return dp[i][j]=1 + maxLength(s1, s2, i - 1, j - 1,dp);
+        }
+            return dp[i][j]=Math.max(maxLength(s1,s2,i-1,j,dp),maxLength(s1,s2,i,j-1,dp));
+    }
+}
